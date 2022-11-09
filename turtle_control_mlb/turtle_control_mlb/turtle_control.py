91934df97a0478d02a6a69c51221914b8f52b7cb
Your Name <you@example.com>
2022-11-09 14:12:17 +0000

    atualizando

--- turtle_control_mlb/turtle_control_mlb/turtle_control.py
+++ turtle_control_mlb/turtle_control_mlb/turtle_control.py
@@ -21,7 +21,6 @@ class TurtleControl(Node):
         self.publisher_ = self.create_publisher(Twist, 'turtle1/cmd_vel', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.pub_callback)
-        self.i = 0
 
     def init_subscribers(self):
         self.subscription = self.create_subscription(
@@ -35,6 +34,7 @@ class TurtleControl(Node):
             'goal',
             self.goal_callback,
             10)
+
         self.subscription  # prevent unused variable warning
 
     def init_variables(self):
@@ -73,7 +73,6 @@ class TurtleControl(Node):
         msg.angular.z = 1.8
 
         self.publisher_.publish(msg)
-        self.i += 1
 
 def main(args=None):
     rclpy.init(args=args)
